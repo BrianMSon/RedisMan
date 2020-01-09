b1b11f140dc330d58779abd3d63bafbfa4f449b7
Cosme Zamudio <cosmez@gmail.com>
2020-01-08 20:28:20 -0700

    Documentation, Command Help WIP

---
+++ RedisMan.Terminal/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RedisMan.Terminal
+{
+    class Program
+    {
+        //
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
