c2ea9eab41d211275bcefdbe1aa7a0eb9f75abfe
Cosme Zamudio <cosmez@gmail.com>
2020-01-10 23:29:14 -0700

    options parsing example

--- RedisMan.Terminal/Program.cs
+++ RedisMan.Terminal/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
+using System.Collections.Generic;
 
 namespace RedisMan.Terminal
 {
@@ -7,7 +9,51 @@ namespace RedisMan.Terminal
         //
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // these variables will be set when the command line is parsed
+            string hostname, password;
+            int port;
+            bool shouldShowHelp = false;
+
+
+            var options = new OptionSet {
+                { "h|hostname=", "the name of someone to greet.", value => { hostname = value; } },
+                { "p|port=", "the number of times to repeat the greeting.", (int p) => port = p },
+                { "a|password=", "increase debug message verbosity", value => { password = value; } },
+                { "help", "show this message and exit", h => shouldShowHelp = h != null },
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("RedisMan.Terminal.exe: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `RedisMan.Terminal.exe --help' for more information.");
+                return;
+            }
+
+
+            if (shouldShowHelp)
+            {
+                ShowHelp(options);
+                return;
+            }
+
+            Console.WriteLine();
         }
+
+
+        private static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("Usage: RedisMan.Terminal.exe [OPTIONS]+ message");
+            Console.WriteLine("Redis Client.");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+
     }
 }
